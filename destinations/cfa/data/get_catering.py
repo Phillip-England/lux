f8800e0aa3f6d0c6f0721df86ee69ff89c6492b7
Phillip-England <Phillip.mark.england@gmail.com>
2022-12-25 06:56:56 -0600

    patched small catering bug

--- destinations/cfa/data/get_catering.py
+++ destinations/cfa/data/get_catering.py
@@ -6,13 +6,14 @@ from util import pdf_to_text
 def get_catering(options):
   
   files = options['files']
+  account = options['account']
   results = {}
 
   for file in files:
 
     if file == 'tomorrow':
       label = 'tomorrow'
-      path = os.path.join(os.environ['PROJECT_PATH'], 'downloads', 'cfa', 'southroads', 'catering', 'tomorrow.pdf')
+      path = os.path.join(os.environ['PROJECT_PATH'], 'downloads', 'cfa', f'{account}', 'catering', 'tomorrow.pdf')
       orders = cfa.data.extract_catering_orders(pdf_to_text(path))
       results[label] = orders
 
